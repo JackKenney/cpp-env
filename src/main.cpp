970433f27198035ee99991e7be73c9c652ec45ca
Jack Kenney <jnkenney@outlook.com>
2020-09-25 16:01:53 -0400

    add using map example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream> // #include = include additional library or file
 #include <vector>
 #include <string>
+#include <random>
+#include <map>
 
 #include "Shape.hpp" // effectively pastes the contents of Shape.hpp here
 #include "Rectangle.hpp"
@@ -9,6 +11,9 @@
 
 using namespace std;
 using namespace Eigen;
+
+// Matrix, ThetaJhat pair
+typedef std::pair<Eigen::MatrixXd, int> pair;
 
 int main(int argc, char **argv)
 {
@@ -39,6 +44,32 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < (int)v.size(); i++)
         cout << v[i] << endl;
+
+    // sorting procedure
+
+    normal_distribution dist(0.0, 5.0);
+    mt19937_64 generator(0);
+    vector<MatrixXd> pop = vector<MatrixXd>(5, MatrixXd::Zero(1, 1));
+    for (int i = 0; i < 5; i++)
+        pop.at(i)(0, 0) = dist(generator);
+    vector<double> sortedOrder{4.4, 5.5, 3.3, 1.1, 2.2};
+
+    cout << "Before" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Order: " << sortedOrder[i] << " Value: " << pop.at(i) << endl;
+    }
+
+    map<double, int> sortingMap;
+    for (int i = 0; i < 5; i++)
+    {
+        sortingMap[sortedOrder.at(i)] = i;
+    }
+    cout << "Using Map:" << endl;
+    for (map<double, int>::iterator i = sortingMap.begin(); i != sortingMap.end(); i++)
+    {
+        cout << "Order: " << i->first << " Place: " << i->second << endl;
+    }
 
     return 0;
 }
